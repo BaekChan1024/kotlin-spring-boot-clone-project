58d5c63cbc5c838bd9196831b6f08c84afecb880
baekchan1024 <baekchan1024@gmail.com>
2023-02-11 16:15:30 +0900

    Initial commit

---
+++ src/main/kotlin/com/study/kotlinspringbootcloneproject/KotlinSpringBootCloneProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.study.kotlinspringbootcloneproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootCloneProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringBootCloneProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/kotlinspringbootcloneproject/KotlinSpringBootCloneProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.kotlinspringbootcloneproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootCloneProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
